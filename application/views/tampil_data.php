5014da26b77fc940ffe37535840a02d4dfb20642
mellyasr <mellymeliasari125@gmail.com>
2021-11-24 17:14:30 +0700

    menambahkan update delete

--- application/views/tampil_data.php
+++ application/views/tampil_data.php
@@ -12,7 +12,7 @@
 			<th>Nama</th>
 			<th>Alamat</th>
 			<th>Pekerjaan</th>
-			<th>Foto</td>
+			<th>Foto</th>
 			<th>Action</th>
 		</tr>
 		<?php
@@ -25,7 +25,7 @@
 			<td><?php echo $u->nama ?></td>
 			<td><?php echo $u->alamat ?></td>
 			<td><?php echo $u->pekerjaan ?></td>
-			<td><img src="<?php echo base_url();?><?php echo $u->foto ?>" width="50" height="50"></td>
+			<td><img src="<?php echo base_url();?>php echo $u->foto ?>" widht="50" height="50"></td>
 			<td>
 				<?php echo anchor('kampus/edit/'.$u->id, 'Edit'); ?>
 				<?php echo anchor('kampus/hapus/'.$u->id, 'Hapus'); ?>
